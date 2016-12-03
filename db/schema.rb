9ce08eb3d0ded5f3f4083ffcf2735c01082eb998
robert <robert.k25@gmail.com>
2016-12-03 14:18:27 -0500

    adds faker and updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202202721) do
+ActiveRecord::Schema.define(version: 20161203185351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20161202202721) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "product_id"
+    t.index ["product_id"], name: "index_comments_on_product_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema.define(version: 20161202202721) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "comments", "products"
 end
