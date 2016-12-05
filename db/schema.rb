d35f685deb270e4247f3afcdfffac56ef2465a67
robert <robert.k25@gmail.com>
2016-12-05 12:19:53 -0500

    adds schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203193639) do
+ActiveRecord::Schema.define(version: 20161205164554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20161203193639) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "imageURL"
+    t.integer  "votes"
   end
 
   add_foreign_key "comments", "products"
