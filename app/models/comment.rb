b1428f86c6d67608b98ae27cf50faeaacd4c018d
robert <robert.k25@gmail.com>
2016-12-05 11:13:55 -0500

    fixes missing end for comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :product
-  validates :body, presence: true
+  validates :body, presence: true, length: {maximum: 100}
   validates :author , presence: true
-  validates :body, length: { maximum: 100}
+end
