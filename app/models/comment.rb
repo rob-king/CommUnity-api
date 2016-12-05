15c2c28a162d1f765d0f13ada6522df10bbd4504
robert <robert.k25@gmail.com>
2016-12-05 12:26:02 -0500

    adjusts comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :product
-  validates :body, presence: true, length: {maximum: 100}
+  validates :body, presence: true, length: {maximum: 1000}
   validates :author , presence: true
 end
