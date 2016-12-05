44025951cbd15c9c22551758ac73abb4be595b94
robert <robert.k25@gmail.com>
2016-12-05 10:35:05 -0500

    adds comments to product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    render :json => @products
+    render json: @products, include: :comments
   end
 
   def show
