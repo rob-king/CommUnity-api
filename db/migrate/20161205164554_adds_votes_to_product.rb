9364c2f94e092f0d6adb643b52e1c562534a5dac
robert <robert.k25@gmail.com>
2016-12-05 13:11:04 -0500

    adds 0 as a the default votes value to products

--- db/migrate/20161205164554_adds_votes_to_product.rb
+++ db/migrate/20161205164554_adds_votes_to_product.rb
@@ -1,5 +1,5 @@
 class AddsVotesToProduct < ActiveRecord::Migration[5.0]
   def change
-    add_column :products, :votes, :integer
+    add_column :products, :votes, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,10 +27,10 @@ ActiveRecord::Schema.define(version: 20161205164554) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "imageURL"
-    t.integer  "votes"
+    t.integer  "votes",       default: 0
   end
 
   add_foreign_key "comments", "products"
