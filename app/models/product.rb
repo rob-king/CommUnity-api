d5aba9519596922963261328a187c62552dbd890
robert <robert.k25@gmail.com>
2016-12-06 11:25:03 -0500

    adds tag and category models to the project

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_many :comments, dependent: :destroy
+  has_many :categories, through: :tags
+  has_many :tags
   validates :name, presence: true
   validates :description, presence: true
   validates :name, length: { maximum: 100 }
