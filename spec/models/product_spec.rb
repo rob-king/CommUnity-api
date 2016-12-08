34cc6a0c92d59b1754a3010c8c31e6c972166ef8
robert <robert.k25@gmail.com>
2016-12-07 21:49:23 -0500

    adds additional product spec skeletons and tag spec skeleton

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe Product, type: :model do
   it "does not allow products with a blank name" do
   end
+  it "does not allow a name longer than 100 characters" do
+  end
   it "does not allow products with a blank description" do
   end
 end
---
+++ spec/models/tags_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Tag, type: :model do
+  it "does not allow a tag without a product" do
+  end
+
+  it "does not allow a tag without a category" do
+  end
+end
