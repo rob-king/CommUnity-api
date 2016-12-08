09f8e89cdc881d11e97c1025fed9e1bfeb98c60a
robert <robert.k25@gmail.com>
2016-12-08 08:52:55 -0500

    moves updating tags logic into the Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,35 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :name, length: { maximum: 100 }
+
+  def update_tags(tags_list)
+    current_tags = self.categories.pluck(:name)
+    tags_to_add = tags_list - current_tags
+    tags_to_remove = current_tags - tags_list
+
+    tags_to_add.each {|tag|
+      create_tag self, tag.downcase
+    }
+
+    tags_to_remove.each{ |tag|
+      remove_tag self, tag.downcase
+    }
+  end
+
+  private
+
+  def create_tag(product, tag_string)
+    category = Category.find_by(name: tag_string)
+    if category
+      product.tags.create(category: category) unless product.categories.pluck(:name).include? tag_string
+    else
+      product.categories.create(name: tag_string)
+    end
+  end
+
+  def remove_tag(product, tag_string)
+    category = Category.find_by(name: tag_string)
+    tag = Tag.find_by(product: product, category: category)
+    tag.destroy
+  end
 end
