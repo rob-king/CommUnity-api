5fe256f6cf276f3ca62845d7e11ef01a06b63555
robert <robert.k25@gmail.com>
2016-12-08 08:41:13 -0500

    adds having a product to comment validation and adds another planned test case

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :product
   validates :body, presence: true, length: {maximum: 1000}
   validates :author , presence: true
+  validates :product, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,5 @@ require "rails_helper"
 RSpec.describe Comment, type: :model do
   it "cannot be created without body content"
   it "cannot be created without an author"
+  it "cannot be created without being associated with a Product"
 end
