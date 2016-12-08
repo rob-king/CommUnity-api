9fdbcb7ae1d7f91800c56fd5318f02cee44799f9
robert <robert.k25@gmail.com>
2016-12-07 21:37:55 -0500

    adds product spec, removes old testing stuff, fixes typo in routes

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe Product, type: :model do
+  it "does not allow products with a blank name" do
+  end
+  it "does not allow products with a blank description" do
+  end
+end
